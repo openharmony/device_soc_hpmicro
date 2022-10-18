fe319105a0070d357537206a697069c85d4e5ec4
Hongpeng Huo <hongpeng.huo@hpmicro.com>
2022-10-18 11:37:13 +0800

    fix license
    - unify copyright
    - remove inactive config from OAT.xml
    
    Signed-off-by: Hongpeng Huo <hongpeng.huo@hpmicro.com>

--- hpm6700/liteos_m/dprintf.c
+++ hpm6700/liteos_m/dprintf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 HPMicro.
+ * Copyright (c) 2022 HPMicro
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
